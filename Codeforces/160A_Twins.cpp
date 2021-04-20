e6f42b1b888c8f09b60f585bbe2664ace687145e
sardonicankita <67410826+sardonicankita@users.noreply.github.com>
2021-04-20 23:50:25 +0530

    Add files via upload

---
+++ Codeforces/160A_Twins.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; 
+    cin>>n;
+    int a[n];
+    int sum=0;
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+     sum=sum/2;
+	 sort(a,a+n);
+	 int sum2=0;
+	 int counter=0;
+	 for(int i=n-1;i>=0;i--){
+		 sum2+=a[i];
+		 ++counter;
+		if(sum2>sum){
+		 	break;
+		}
+	 }
+
+	cout<<counter;
+	return 0;
+}
