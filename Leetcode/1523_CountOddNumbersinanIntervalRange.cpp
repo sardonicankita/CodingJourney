dbcdea329f0f01562fb0c63b04f6f95f4417eed2
sardonicankita <ankitaprasad67@gmail.com>
2021-06-04 11:41:17 +0530

    Day3cpp

---
+++ Leetcode/1523_CountOddNumbersinanIntervalRange.cpp
@@ -0,0 +1,13 @@
+1523_CountOddNumbersinanIntervalRange
+//EASY
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+      int ans=(high-low)/2;
+        if(low%2!=0 ||high%2!=0)
+            ans++;
+        return ans;
+    }
+    
+};
